37ebc60b71317da3de3493b26adfb8106bb9b66f
vishnup <vishnup@zenoti.com>
2024-03-27 19:44:37 +0530

    add initial auth setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button size='sm' variant='destructive'>Click Me</Button>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   );
 }
