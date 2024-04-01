0452e6efdc84259a49f8e4ad2eb0aad742aeb534
vishnup <vishnup@zenoti.com>
2024-04-01 10:23:58 +0530

    add navbar

---
+++ app/(browse)/(home)/_components/navbar/index.tsx
@@ -0,0 +1,15 @@
+import Search from './search';
+import Actions from './actions';
+import { Logo } from './logo';
+
+const Navbar = () => {
+  return (
+    <nav className='fixed top-0 w-full h-20 z-[49] bg-[#252731] px-2 lg:px-4 flex justify-between items-center shadow-sm'>
+      <Logo />
+      <Search />
+      <Actions />
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ app/(browse)/(home)/_components/navbar/logo.tsx
@@ -0,0 +1,25 @@
+import { cn } from '@/lib/utils';
+import { Poppins } from 'next/font/google';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '600', '700', '800'],
+});
+
+export const Logo = () => {
+  return (
+    <Link href='/'>
+      <div className='flex lg:flex items-center gap-x-4 hover:opacity-75 transition'>
+        <div className='bg-white rounded-full p-1 mr-12 shrink-0 lg:mr-0 lg:shrink'>
+          <Image src='/spooky.svg' alt='Gamehub' height={32} width={32} />
+        </div>
+        <div className={cn('hidden lg:block',font.className)}>
+          <p className='text-lg font-semibold'>Gamehub</p>
+          <p className='text-sm text-muted-foreground'>Let&apos;s play</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
