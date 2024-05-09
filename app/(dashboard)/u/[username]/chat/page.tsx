c8782da5f4265908a4470e153f4360d866609cc7
vishnup <vishnup@zenoti.com>
2024-05-09 15:00:43 +0530

    remove map and hardcode data

--- app/(dashboard)/u/[username]/chat/page.tsx
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -2,9 +2,9 @@ import { getSelf } from '@/lib/auth-service';
 import { getStreamByUserId } from '@/lib/stream-service';
 import { ToggleCard, ToggleCardProps } from './_components/toggle-card';
 
-interface ExtendedToggleCardProps extends ToggleCardProps {
+/* interface ExtendedToggleCardProps extends ToggleCardProps {
   key: string;
-}
+} */
 
 const ChatPage = async () => {
   const self = await getSelf();
@@ -14,7 +14,7 @@ const ChatPage = async () => {
     throw new Error('Stream not found');
   }
 
-  const settings: ExtendedToggleCardProps[] = [
+  /* const settings: ExtendedToggleCardProps[] = [
     {
       key: 'isChatEnabled',
       field: 'isChatEnabled',
@@ -33,7 +33,7 @@ const ChatPage = async () => {
       label: 'Must be following to chat',
       value: stream.isChatFollowersOnly,
     },
-  ];
+  ]; */
 
   return (
     <div className='p-6'>
@@ -41,9 +41,24 @@ const ChatPage = async () => {
         <h1 className='text-2xl font-bold'>Chat Settings</h1>
       </div>
       <div className='space-y-4'>
-        {settings.map((setting) => (
+        {/* {settings.map((setting) => (
           <ToggleCard {...setting} key={setting.key} />
-        ))}
+        ))} */}
+        <ToggleCard
+          field='isChatEnabled'
+          label='Enable Chat'
+          value={stream.isChatEnabled}
+        />
+        <ToggleCard
+          field='isChatDelayed'
+          label='Delay Chat'
+          value={stream.isChatDelayed}
+        />
+        <ToggleCard
+          field='isChatFollowersOnly'
+          label='Must be following to chat'
+          value={stream.isChatFollowersOnly}
+        />
       </div>
     </div>
   );
